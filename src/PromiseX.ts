1b3b38dd4154e0607646a8d8c0635f0d90f601c7
Lin Luo <l.luo-pi@outlook.com>
2018-01-11 15:40:19 -0800

    'await'able PromiseX

--- src/PromiseX.ts
+++ src/PromiseX.ts
@@ -1,41 +1,92 @@
-export class PromiseX<T = any> {
+function isPromiseLike<T>(x: any): x is PromiseLike<T> {
+    return x != null && 'then' in x;
+}
+
+enum State { Pending, Fulfilled, Rejected }
+
+export class PromiseX<T = any> implements PromiseLike<T> {
     private _result: T;
-    private _continuations: ((x: T) => void)[] = [];
+    private _continuations: [(x: T) => void, (e: any) => void][] = [];
 
-    constructor(executor?: (resolve: (x: T) => void) => void) {
+    private _reason: any;
+
+    private _state: State = State.Pending;
+
+    constructor(executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void) {
         if (executor) {
-            executor(result => this.setResult(result)); // NB: cannot pass this.setResult directly!
+            executor(result => this.setResult(result), error => this.setError(error));
         }
     }
 
-    public setResult(result: T): void {
-        if (this._result == null && result != null) { // 'null' or 'undefined'
-            this._result = result;
-            for (let continuation of this._continuations) {
-                continuation(this._result);
-            }
-        }
-    }
-
-    public then(continuation: (x: T) => PromiseX<T> | T | void): PromiseX<T> {
-        if (this._result != null) {
-            let r = continuation(this._result);
-            if (r instanceof PromiseX) {
-                return r;
-            }
-            let p = new PromiseX<T>();
-            p.setResult(r != null ? r as T : this._result);
-            return p;
-        }
-        let p = new PromiseX<T>();
-        this._continuations.push(result => {
-            let r = continuation(result);
-            if (r instanceof PromiseX) {
-                r.then(x => p.setResult(x));
+    // if 'result' is 'undefined', it'll be passed along
+    public setResult(result?: T | PromiseLike<T>): void {
+        if (this._state == State.Pending) {
+            this._state = State.Fulfilled;
+            if (isPromiseLike(result)) {
+                result.then(x => this.setResult(x));
             } else {
-                p.setResult(r != null ? r as T : this._result);
+                this._result = result;
+                for (let continuation of this._continuations) {
+                    continuation[0](this._result);
+                }
             }
-        });
-        return p;
+        }
+    }
+
+    public setError(reason?: any): void {
+        if (this._state == State.Pending) {
+            this._state = State.Rejected;
+            this._reason = reason;
+            for (let continuation of this._continuations) {
+                continuation[1](this._reason);
+            }
+        }
+    }
+
+    then<TResult1 = T, TResult2 = never>(onFulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onRejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): PromiseLike<TResult1 | TResult2> {
+        switch (this._state) {
+            case State.Fulfilled: {
+                let r = onFulfilled ? onFulfilled(this._result) : undefined;
+                if (isPromiseLike(r)) {
+                    return r;
+                }
+                let p = new PromiseX<TResult1>(undefined);
+                p.setResult(r);
+                return p;
+            }
+
+            case State.Rejected: {
+                let r = onRejected ? onRejected(this._reason) : undefined;
+                if (isPromiseLike(r)) {
+                    return r;
+                }
+                let p = new PromiseX<TResult2>(undefined);
+                p.setError(r);
+                return p;
+            }
+
+            case State.Pending: {
+                let p = new PromiseX<TResult1 | TResult2>(undefined);
+                this._continuations.push([
+                    result => {
+                        let r = onFulfilled ? onFulfilled(result) : undefined;
+                        if (isPromiseLike(r)) {
+                            r.then(x => p.setResult(x));
+                        } else {
+                            p.setResult(r);
+                        }
+                    },
+                    reason => {
+                        let r = onRejected ? onRejected(reason) : undefined;
+                        if (isPromiseLike(r)) {
+                            r.then(x => p.setError(x));
+                        } else {
+                            p.setError(r);
+                        }
+                    }
+                ]);
+                return p;
+            }
+        }
     }
 }
--- src/Test.ts
+++ src/Test.ts
@@ -4,44 +4,58 @@ console.log("hello TypeScript!");
 
 setTimeout(console.log, 1000, "xxxxxx");
 
-function ComputeAsyncX(x: number) : PromiseX<number> {
-    return new PromiseX(resolve => setTimeout(resolve, 1000, x + 1));
+// function ComputeAsyncX(x: number) : PromiseX<number> {
+//     return new PromiseX(resolve => setTimeout(resolve, 1000, x + 1));
+// }
+
+// ComputeAsyncX(0)
+//     .then(console.log)
+//     .then(ComputeAsyncX)
+//     .then(r => { console.log(r); return ComputeAsyncX(r); })
+//     .then(r => { console.log(r); return ComputeAsyncX(r); })
+//     .then(r => { console.log(r); return r + 1; })
+
+//     .then(r => { console.log(r); return ComputeAsyncX(r); })
+//     .then(console.log)
+//     .then(ComputeAsyncX)
+//     .then(console.log)
+//     .then(ComputeAsyncX)
+//     .then(console.log)
+//     .then(x => 233)
+//     .then(console.log)
+//     ;
+
+// var p = new PromiseX();
+// p.setResult(42);
+// p.then(console.log)
+//     .then(v => "xyz")
+//     .then(console.log)
+//     .then(ComputeAsyncX)
+//     .then(console.log)
+//     .then(x => 999)
+//     .then(console.log)
+//     ;
+
+// var p2 = new PromiseX();
+// p2.then(ComputeAsyncX).then(console.log);
+// p2.then(ComputeAsyncX).then(console.log);
+// p2.setResult(2000);
+// p2.then(ComputeAsyncX).then(console.log);
+// p2.then(ComputeAsyncX).then(console.log);
+
+function delay(ms: number) : PromiseX
+{
+    return new PromiseX(resolve => setTimeout(resolve, ms));
 }
 
-ComputeAsyncX(0)
-    .then(console.log)
-    .then(ComputeAsyncX)
-    .then(r => { console.log(r); return ComputeAsyncX(r); })
-    .then(r => { console.log(r); return ComputeAsyncX(r); })
-    .then(r => { console.log(r); return r + 1; })
+async function TestAsync() : PromiseX
+{
+    console.log("TestAsync: start");
+    await delay(1000);
+    console.log("TestAsync: done");
+}
 
-    .then(r => { console.log(r); return ComputeAsyncX(r); })
-    .then(console.log)
-    .then(ComputeAsyncX)
-    .then(console.log)
-    .then(ComputeAsyncX)
-    .then(console.log)
-    .then(x => 233)
-    .then(console.log)
-    ;
-
-var p = new PromiseX();
-p.setResult(42);
-p.then(console.log)
-    .then(v => "xyz")
-    .then(console.log)
-    .then(ComputeAsyncX)
-    .then(console.log)
-    .then(x => 999)
-    .then(console.log)
-    ;
-
-var p2 = new PromiseX();
-p2.then(ComputeAsyncX).then(console.log);
-p2.then(ComputeAsyncX).then(console.log);
-p2.setResult(2000);
-p2.then(ComputeAsyncX).then(console.log);
-p2.then(ComputeAsyncX).then(console.log);
+TestAsync();
 
 var a : any[] = [];
 a.push(1, "hello", true);
