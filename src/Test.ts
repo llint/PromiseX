5ae7591fecd324f51690239ad0e1402a89072cfd
Lin Luo <l.luo-pi@outlook.com>
2018-01-11 17:02:44 -0800

    Adding back the test cases

--- src/Test.ts
+++ src/Test.ts
@@ -4,46 +4,49 @@ console.log("hello TypeScript!");
 
 setTimeout(console.log, 1000, "xxxxxx");
 
-// function ComputeAsyncX(x: number) : PromiseX<number> {
-//     return new PromiseX(resolve => setTimeout(resolve, 1000, x + 1));
-// }
+function ComputeAsyncX(x: number) : PromiseLike<number> {
+    return new PromiseX(resolve => setTimeout(resolve, 1000, x + 1));
+}
 
-// ComputeAsyncX(0)
-//     .then(console.log)
-//     .then(ComputeAsyncX)
-//     .then(r => { console.log(r); return ComputeAsyncX(r); })
-//     .then(r => { console.log(r); return ComputeAsyncX(r); })
-//     .then(r => { console.log(r); return r + 1; })
+ComputeAsyncX(0)
+    .then(console.log)
+    // .then(ComputeAsyncX) - very nicely, this is not allowed, since the previous then returns void
+    .then(() => ComputeAsyncX(10))
+    .then(r => { console.log(r); return ComputeAsyncX(r); })
+    // .then(r => { console.log(r); return ComputeAsyncX(r); })
+    .then(r => { console.log(r); return r + 1; })
 
-//     .then(r => { console.log(r); return ComputeAsyncX(r); })
-//     .then(console.log)
-//     .then(ComputeAsyncX)
-//     .then(console.log)
-//     .then(ComputeAsyncX)
-//     .then(console.log)
-//     .then(x => 233)
-//     .then(console.log)
-//     ;
+    .then(r => { console.log(r); return ComputeAsyncX(r); })
+    .then(console.log)
+    // .then(ComputeAsyncX)
+    .then(() => ComputeAsyncX(20))
+    .then(console.log)
+    // .then(ComputeAsyncX)
+    .then(() => ComputeAsyncX(30))
+    .then(console.log)
+    .then(x => 233)
+    .then(console.log)
+    ;
 
-// var p = new PromiseX();
-// p.setResult(42);
-// p.then(console.log)
-//     .then(v => "xyz")
-//     .then(console.log)
-//     .then(ComputeAsyncX)
-//     .then(console.log)
-//     .then(x => 999)
-//     .then(console.log)
-//     ;
+var p = new PromiseX();
+p.setResult(42);
+p.then(console.log)
+    .then(v => "xyz")
+    .then(console.log)
+    .then(() => ComputeAsyncX(40))
+    .then(console.log)
+    .then(x => 999)
+    .then(console.log)
+    ;
 
-// var p2 = new PromiseX();
-// p2.then(ComputeAsyncX).then(console.log);
-// p2.then(ComputeAsyncX).then(console.log);
-// p2.setResult(2000);
-// p2.then(ComputeAsyncX).then(console.log);
-// p2.then(ComputeAsyncX).then(console.log);
+var p2 = new PromiseX();
+p2.then(ComputeAsyncX).then(console.log);
+p2.then(ComputeAsyncX).then(console.log);
+p2.setResult(2000);
+p2.then(ComputeAsyncX).then(console.log);
+p2.then(ComputeAsyncX).then(console.log);
 
-function delay(ms: number) : PromiseX<string>
+function delay(ms: number) : PromiseLike<string>
 {
     return new PromiseX(resolve => setTimeout(resolve, ms, "fdafdafda"));
 }
